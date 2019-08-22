b07101784fd122d98c79deca009b3ef900bb5465
Jonathan Lassen <jonathan.lassen@gmail.com>
2019-08-21 23:08:11 -0400

    cleaning up

--- src/app.js
+++ src/app.js
@@ -1,32 +1,24 @@
+/* eslint-disable strict */
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-// addind to db here
-
-
-
 const validateBearerToken = require('./validate-bearer-token')
 const errorHandler = require('./error-handler')
 const LeafRouter = require('./leaf/leaf-router')
-
 const app = express()
-
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test'
 }))
 app.use(cors())
 app.use(helmet())
 app.use(validateBearerToken)
-
 app.use(LeafRouter)
-
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
 app.use(errorHandler)
-
 module.exports = app
--- src/leaf/leaf-router.js
+++ src/leaf/leaf-router.js
@@ -1,35 +1,38 @@
-const express = require('express')
-const uuid = require('uuid/v4')
-const { isWebUri } = require('valid-url')
-const logger = require('../logger')
-const store = require('../store')
-const LeafService = require('./leaf-service')
-const AuthService = require('./auth-service')
-const bcrypt = require('bcryptjs')
+/* eslint-disable strict */
+const express = require('express');
+const uuid = require('uuid/v4');
+const { isWebUri } = require('valid-url');
+const logger = require('../logger');
+const store = require('../store');
+const LeafService = require('./leaf-service');
+const AuthService = require('./auth-service');
+const bcrypt = require('bcryptjs');
 const LeafRouter = express.Router();
-const bodyParser = express.json()
-const { requireAuth } = require('./jwt-auth')
+const bodyParser = express.json();
+const { requireAuth } = require('./jwt-auth');
 
 const serializeLeaf = leaf => ({
   id: leaf.id,
   title: leaf.name,
   lat: leaf.lat,
   long: leaf.long,
-})
+  zip: leaf.zip
+});
 
 const serializeUser = (user, token) => ({
   id: user.id,
   username: user.username,
   email: user.email,
-  token:token
-})
+  token:token,
+  shop_id: user.shop_id
+});
 
 const serializeShop = shop => ({
   name: shop.name,
   telephone: shop.telephone,
   statecode: shop.statecode,
   zip:shop.zip
-})
+});
 
 LeafRouter
   .route('/login')
@@ -37,28 +40,27 @@ LeafRouter
     // TODO: update to use db
     for (const field of ['password', 'username']) {
       if (!req.body[field]) {
-        logger.error(`${field} is required`)
-        return res.status(400).send(`'${field}' is required`)
+        logger.error(`${field} is required`);
+        return res.status(400).send(`'${field}' is required`);
       }
     }
 
     const { username, password } = req.body;
-
     AuthService.getUserWithUserName(req.app.get('db'), username).then(user => {
       if (!user) {
-        return res.status(401).json({ error: 'Unauthorized request1' })
+        return res.status(401).json({ error: 'Unauthorized request1' });
       }
 
       return bcrypt.compare(password, user.password)
-            .then(passwordsMatch => {
-              if (!passwordsMatch) {
-                return res.status(401).json({ error: 'Unauthorized request2' })
-              }
-              const sub = user.username
-              const payload = { id: user.id }
-              const token = AuthService.createJwt(sub, payload);
-              res.json(serializeUser(user, token))
-            });
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: 'Unauthorized request2' });
+          }
+          const sub = user.username;
+          const payload = { id: user.id };
+          const token = AuthService.createJwt(sub, payload);
+          res.json(serializeUser(user, token));
+        });
     });
   });
 
@@ -68,122 +70,101 @@ LeafRouter
     // TODO: update to use db
     for (const field of ['password', 'username']) {
       if (!req.body[field]) {
-        logger.error(`${field} is required`)
-        return res.status(400).send(`'${field}' is required`)
+        logger.error(`${field} is required`);
+        return res.status(400).send(`'${field}' is required`);
       }
     }
     // check for existing email/username
   
     const { username, password, email } = req.body;
     bcrypt.hash(password, 12).then(hash => {
-        const user = { username, password:hash, email};
-        AuthService.insertUser(req.app.get('db'), user);
+      const user = { username, password:hash, email};
+      AuthService.insertUser(req.app.get('db'), user);
     }
     );
   });
 
-  LeafRouter
+LeafRouter
   .route('/claim')
   .post(bodyParser, (req, res) => {
     const { user, id } = req.body;
-        console.log(user.id, id)
-      AuthService.claimShop(req.app.get('db'), user.id, id).then(shop => {
-  
-      res.json(shop)
-
+    console.log(user.id, id);
+    AuthService.claimShop(req.app.get('db'), user.id, id).then(shop => {
+      res.json(shop);
     });
-
-
   });
-
-
 
 LeafRouter
   .route('/Leaf')
   .get((req, res, next) => {
     LeafService.getAllLeaf(req.app.get('db'))
       .then(Leaf => {
-        res.json(Leaf.map(serializeLeaf))
+        res.json(Leaf.map(serializeLeaf));
       })
-      .catch(next)
+      .catch(next);
   })
   .post(bodyParser, (req, res) => {
     // TODO: update to use db
     for (const field of ['title', 'url', 'rating']) {
       if (!req.body[field]) {
-        logger.error(`${field} is required`)
-        return res.status(400).send(`'${field}' is required`)
+        logger.error(`${field} is required`);
+        return res.status(400).send(`'${field}' is required`);
       }
     }
-    const { title, url, description, rating } = req.body
-
+    const { title, url, description, rating } = req.body;
     if (!isWebUri(url)) {
-      logger.error(`Invalid url '${url}' supplied`)
-      return res.status(400).send(`'url' must be a valid URL`)
+      logger.error(`Invalid url '${url}' supplied`);
+      return res.status(400).send('\'url\' must be a valid URL');
     }
-
-    const leaf = { id: uuid(), title, url, description, rating }
-
-    store.Leaf.push(leaf)
-
-    logger.info(`Leaf with id ${leaf.id} created`)
+    const leaf = { id: uuid(), title, url, description, rating };
+    store.Leaf.push(leaf);
+    logger.info(`Leaf with id ${leaf.id} created`);
     res
       .status(201)
       .location(`http://localhost:8000/Leaf/${leaf.id}`)
-      .json(leaf)
-  })
+      .json(leaf);
+  });
 
 LeafRouter
   .route('/shop/:id')
   .get((req, res, next) => {
-    const { id } = req.params
+    const { id } = req.params;
     LeafService.getById(req.app.get('db'), id)
       .then(shop => {
         if (!shop) {
-          logger.error(`shop with id ${leaf_id} not found.`)
+          logger.error(`shop with id ${id} not found.`);
           return res.status(404).json({
-            error: { message: `shop Not Found` }
-          })
+            error: { message: 'shop Not Found' }
+          });
         }
-        res.json(shop)
+        res.json(shop);
       })
-      .catch(next)
+      .catch(next);
   })
   .patch(bodyParser, (req, res) => {
     // TODO: update to use db
-    const { id } = req.params
-    const { name, telephone, statecode, zip } = req.body
-    const shop = { name, telephone, statecode, zip }
-
-
+    const { id } = req.params;
+    const { name, telephone, statecode, zip } = req.body;
+    const shop = { name, telephone, statecode, zip };
     LeafService.updateShop(req.app.get('db'), id, serializeShop(shop)).then(shop => {
-  
-      res.json(shop)
-
+      res.json(shop);
     });
-
-
-
   })
   .delete((req, res) => {
     // TODO: update to use db
-    const { leaf_id } = req.params
-
-    const LeafIndex = store.Leaf.findIndex(b => b.id === leaf_id)
-
+    const { leaf_id } = req.params;
+    const LeafIndex = store.Leaf.findIndex(b => b.id === leaf_id);
     if (LeafIndex === -1) {
-      logger.error(`leaf with id ${leaf_id} not found.`)
+      logger.error(`leaf with id ${leaf_id} not found.`);
       return res
         .status(404)
-        .send('leaf Not Found')
+        .send('leaf Not Found');
     }
-
-    store.Leaf.splice(LeafIndex, 1)
-
-    logger.info(`leaf with id ${leaf_id} deleted.`)
+    store.Leaf.splice(LeafIndex, 1);
+    logger.info(`leaf with id ${leaf_id} deleted.`);
     res
       .status(204)
-      .end()
-  })
+      .end();
+  });
 
-module.exports = LeafRouter
+module.exports = LeafRouter;
